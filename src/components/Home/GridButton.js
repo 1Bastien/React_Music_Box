b03ceebcb7b3f6b77dd70c404bb91aabc0a28a28
Popeye <bastien.bourgeat1@gmail.com>
2023-02-12 14:15:38 +0100

    A-Grid button (Hagrid, tu l'as ?)

---
+++ src/components/Home/GridButton.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export default function GridButton () {
+    return (
+        <Wrapper>
+
+        </Wrapper>
+    );
+}
+
+const Wrapper = styled.div`
+    border: solid 1px;
+`;
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,32 @@
 import styled from "styled-components";
+import GridButton from "./GridButton";
+
+const list = ["el1", "el2", "el3", "el4"];
 
 export default function Home () {
     return (
         <Wrapper>
-            <p>home</p>
+            <Grid>{list.map((el)=>{
+                return <GridButton key={el}>{el}</GridButton>
+            })}</Grid>
         </Wrapper>
     );
 }
 
 const Wrapper = styled.div``;
+
+const Grid = styled.div`
+display: grid;
+width: 400px;
+height: 400px;
+border solid 1px;
+grid-template-columns: 1fr 1fr;
+column-gap: 12px;
+row-gap: 12px;
+padding: 24px;
+margin: auto;
+@media (max-width: 640px) {
+    width: 400px;
+    height: 400px;
+}
+`;
